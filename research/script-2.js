9691cb63cf6b925a31ceb6318d5f46e044354979
catherinerk <46656491+catherinerk@users.noreply.github.com>
2019-01-23 01:22:29 -0800

    Add files via upload

--- research/script-2.js
+++ research/script-2.js
@@ -12,25 +12,40 @@ console.log('reading.js');
 
 // }
 
+var i;
+var c;
+
 function setup() {
   //size is createCanvas in p5
   //create a variable to reference to createCanvas
   var myCanvas = createCanvas(800, 250);
   myCanvas.parent('mySketch');
+
+  i=0;
+  c = ['#ffaa97','#becbd4', '#fdcaaa','#ff7f78', '#ffffff'];
 }
 
 function draw() {
   // stroke(255);
   // ellipse(mouseX, mouseY, 100, 100);
   // noFill();
- for(var x = 0; x<= width; x +=50){
+
+ // for(var x = 0; x<= width; x +=50){
  	stroke('#ffffff');
-  	triangle(mouseX - 50, mouseY + 25, mouseX + 50, mouseY, mouseX + 25, mouseY + 100);
-  	
-  	var c = ['#ffffff','#becbd4', '#fdcaaa','#ff7f78'];
-  	for(var i=0; i<4; i++){
+
+
+  	// for(var i=0; i<c.length; i++){
+
+    if (i<c.length-1){
+        i++;
+      } else {
+        i=0;
+      }
+
   		fill(c[i]);
-  	}
-}
+      triangle(mouseX - 50, mouseY + 25, mouseX + 50, mouseY, mouseX + 25, mouseY + 100);
+
+  	// }
+// }
 
 }
