74d516de6ccdbedfca6a71056e7fd9014b1026a0
catherinerk <46656491+catherinerk@users.noreply.github.com>
2019-02-05 23:18:44 -0800

    Add files via upload

---
+++ studio2/script.js
@@ -0,0 +1,45 @@
+console.log('reading js');
+// use 'strict';
+
+var bp = document.querySelector('#bp');
+var overlay = document.querySelector('#overlay');
+var close = document.querySelector('#close');
+var ipad = document.querySelector('#ipad');
+var overlay2 = document.querySelector('#overlay2');
+var close2 = document.querySelector('#close2');
+var headphones = document.querySelector('#headphones');
+var overlay3 = document.querySelector('#overlay3');
+var close3 = document.querySelector('#close3');
+
+document.getElementById('bp').addEventListener('click',function(){
+ overlay.style.display='block';
+});
+
+
+document.getElementById('close').addEventListener('click',function(){
+  overlay.style.display='none';
+});
+
+document.getElementById('ipad').addEventListener('click',function(){
+ overlay2.style.display='block';
+});
+
+  document.getElementById('close2').addEventListener('click',function(){
+    overlay2.style.display='none';
+});
+
+document.getElementById('headphones').addEventListener('click',function(){
+ overlay3.style.display='block';
+});
+
+  document.getElementById('close3').addEventListener('click',function(){
+    overlay3.style.display='none';
+});
+
+document.getElementById('chapstick').addEventListener('click',function(){
+ overlay4.style.display='block';
+});
+
+  document.getElementById('close4').addEventListener('click',function(){
+    overlay4.style.display='none';
+});
